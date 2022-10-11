ef95f2a0af4eb68d1e3f05b2a4d438dd0f19d14d
ReArmel <rearmel@icloud.com>
2022-10-11 17:47:17 -0400

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+using DesignPatterns;
+
+
+public class Program
+{
+     static void Main(string[] args)
+    {
+                
+            Imposto iss = new ISS();
+            Imposto icms = new ICMS();
+
+            Orcamento orcamento = new Orcamento(5000.0);
+            CalculadorDeImpostos calculador = new CalculadorDeImpostos();
+
+        calculador.RealizaCalculo(orcamento, icms);
+        calculador.RealizaCalculo(orcamento, iss);
+        Console.ReadKey();
+
+        
+    }
+}
